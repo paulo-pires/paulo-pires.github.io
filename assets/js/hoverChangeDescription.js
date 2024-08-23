96f75dcd286ae2f5e049fd41762166ac1f3ff5c3
paulo pires <srpaulo.hp@gmail.com>
2024-08-23 20:26:03 -0300

    tradução

--- assets/js/hoverChangeDescription.js
+++ assets/js/hoverChangeDescription.js
@@ -1,11 +1,38 @@
-export function hoverChangeDescription(nameCard, text) {
-  var changeDescription = document.querySelector(".changeDescription");
+const descriptions = {
+  'pt-br': {
+    python: "Python é uma linguagem de programação de alto nível, conhecida por sua simplicidade e legibilidade, amplamente utilizada em desenvolvimento web, ciência de dados, automação e muito mais.",
+    angular: "Angular é uma plataforma e framework para construir aplicações web dinâmicas e robustas, utilizando TypeScript como linguagem principal.",
+    php: "PHP é uma linguagem de script do lado do servidor, especialmente adequada para desenvolvimento web e pode ser embutida em HTML.",
+    js: "JavaScript é uma linguagem de programação versátil que permite a criação de elementos dinâmicos e interativos em páginas web.",
+    react: "React é uma biblioteca JavaScript para construção de interfaces de usuário, baseada em componentes reutilizáveis.",
+    next: "Next.js é um framework React que habilita funcionalidades como renderização do lado do servidor e geração de sites estáticos.",
+    tailwind: "Tailwind CSS é um framework de utilitários CSS que permite a criação rápida de interfaces estilizadas diretamente no HTML.",
+    typescript: "TypeScript é um superset do JavaScript que adiciona tipagem estática e outros recursos avançados, facilitando a manutenção e escalabilidade do código."
+  },
+  'en': {
+    python: "Python is a high-level programming language known for its simplicity and readability, widely used in web development, data science, automation, and more.",
+    angular: "Angular is a platform and framework for building dynamic and robust web applications, using TypeScript as the main language.",
+    php: "PHP is a server-side scripting language especially suited for web development and can be embedded into HTML.",
+    js: "JavaScript is a versatile programming language that enables the creation of dynamic and interactive elements on web pages.",
+    react: "React is a JavaScript library for building user interfaces, based on reusable components.",
+    next: "Next.js is a React framework that enables functionalities like server-side rendering and static site generation.",
+    tailwind: "Tailwind CSS is a utility-first CSS framework that allows for rapid creation of styled interfaces directly in HTML.",
+    typescript: "TypeScript is a superset of JavaScript that adds static typing and other advanced features, making code maintenance and scalability easier."
+  }
+};
+export function hoverChangeDescription(nameCard, key, lang = 'pt-br') {
+  const changeDescription = document.querySelector(".changeDescription");
+
+  if (!changeDescription) {
+    console.error("Element with class 'changeDescription' not found.");
+    return;
+  }
 
   document.querySelector(nameCard).addEventListener("mouseover", () => {
-    changeDescription.innerHTML = text;
+    changeDescription.innerHTML = descriptions[lang][key];
   });
 
   document.querySelector(nameCard).addEventListener("mouseout", () => {
-    changeDescription.innerHTML = `*passe o cursor do mouse no card para ler*`;
+    changeDescription.innerHTML = lang === 'pt-br' ? `*passe o cursor do mouse no card para ler*` : `*hover over the card to read*`;
   });
 }
--- assets/js/hoverChangeExperience.js
+++ assets/js/hoverChangeExperience.js
@@ -1,20 +1,84 @@
-export function hoverChangeExperience(
-  nameCard,
-  changeDescription,
-  titleExperience,
-  companyExperience,
-  dateExperience
-) {
+const experiences = {
+  'pt-br': {
+    geosapiens: {
+      description: `Durante meu período como trainee, aprendi e trabalhei com PHP e Symfony, além de desenvolver habilidades em Java e PostgreSQL. Também adquiri conhecimentos em virtualização com Docker e localização geoespacial, que ampliaram minha compreensão de tecnologias de banco de dados e suas aplicações práticas de performance.`,
+      title: "Trainee em Desenvolvimento Web",
+      company: "GeoSapiens",
+      date: "Fev 2018 - Fev 2019 (1 ano)"
+    },
+    convert: {
+      description: `Trabalhei na Convert, onde me concentrei na criação de integrações API para plataformas de e-commerce com Magento. Meu principal objetivo era melhorar o desempenho das integrações, garantindo uma comunicação eficiente entre os sistemas e otimizando a performance geral das plataformas.`,
+      title: "Desenvolvedor de Integrações API",
+      company: "Convert",
+      date: "Mar 2019 - Mar 2020 (1 ano)"
+    },
+    pulses: {
+      description: `Implementei soluções de OLTP com SingleStore e MemSQL para otimização de bancos de dados, realizei melhorias no código PHP Laravel e implementei testes automatizados com Cypress.`,
+      title: "Consultor de melhoria de performance",
+      company: "Pulses Gupy",
+      date: "Abr 2020 - Abr 2021 (1 ano)"
+    },
+    ani: {
+      description: `Trabalhei como gerenciamento de frotas, onde apliquei meu conhecimento full stack em NoSQL, desenvolvi back-end em Node.js, front-end em Angular, e criei aplicativos móveis usando Angular e Cordova para solucionar desafios da empresa.`,
+      title: "Desenvolvedor Full Stack",
+      company: "A.N.I Sistemas",
+      date: "Mai 2021 - Mai 2023 (2 anos)"
+    },
+    foccos: {
+      description: `Atualmente, estou em uma empresa na área de gestão e controle de acesso, atuando como desenvolvedor full-stack. Aplicando meus conhecimentos em back-end com Next.js e front-end com React, além de trabalhar com soluções de visão computacional e inteligência artificial utilizando Python. Também gerencio um servidor Windows com banco de dados SQL Server.`,
+      title: "Desenvolvedor Full Stack",
+      company: "Foccos",
+      date: "Jun 2023 - Presente"
+    }
+  },
+  'en': {
+    geosapiens: {
+      description: `During my trainee period, I learned and worked with PHP and Symfony, as well as developed skills in Java and PostgreSQL. I also gained knowledge in virtualization with Docker and geospatial localization, which expanded my understanding of database technologies and their practical performance applications.`,
+      title: "Web Development Trainee",
+      company: "GeoSapiens",
+      date: "Feb 2018 - Feb 2019 (1 year)"
+    },
+    convert: {
+      description: `I worked at Convert, where I focused on creating API integrations for e-commerce platforms with Magento. My main goal was to improve the performance of integrations, ensuring efficient communication between systems and optimizing the overall performance of the platforms.`,
+      title: "API Integrations Developer",
+      company: "Convert",
+      date: "Mar 2019 - Mar 2020 (1 year)"
+    },
+    pulses: {
+      description: `I implemented OLTP solutions with SingleStore and MemSQL for database optimization, made improvements to PHP Laravel code, and implemented automated tests with Cypress.`,
+      title: "Performance Improvement Consultant",
+      company: "Pulses Gupy",
+      date: "Apr 2020 - Apr 2021 (1 year)"
+    },
+    ani: {
+      description: `I worked in fleet management, where I applied my full stack knowledge in NoSQL, developed back-end in Node.js, front-end in Angular, and created mobile applications using Angular and Cordova to solve company challenges.`,
+      title: "Full Stack Developer",
+      company: "A.N.I Sistemas",
+      date: "May 2021 - May 2023 (2 years)"
+    },
+    foccos: {
+      description: `Currently, I am at a company in the area of management and access control, working as a full-stack developer. Applying my knowledge in back-end with Next.js and front-end with React, as well as working with computer vision and artificial intelligence solutions using Python. I also manage a Windows server with SQL Server database.`,
+      title: "Full Stack Developer",
+      company: "Foccos",
+      date: "Jun 2023 - Present"
+    }
+  }
+};
+
+export function hoverChangeExperience(nameCard, key, lang = 'pt-br') {
   const varChangeDescription = document.querySelector(".changeExperience");
-  const varTitleExperience = document.querySelector(".titleExperience");
   const varCompanyExperience = document.querySelector(".companyExperience");
   const varDateExperience = document.querySelector(".dateExperience");
+  
+  if (!varChangeDescription  || !varCompanyExperience || !varDateExperience) {
+    console.error("One or more elements not found in the DOM.");
+    return;
+  }
 
   document.querySelector(nameCard).addEventListener("click", () => {
-    varChangeDescription.innerHTML = changeDescription;
-    varCompanyExperience.innerHTML = companyExperience;
-    varTitleExperience.innerHTML = titleExperience;
-    varDateExperience.innerHTML = dateExperience;
+    varChangeDescription.innerHTML = experiences[lang][key].description;
+    varCompanyExperience.innerHTML = experiences[lang][key].company;
+    varDateExperience.innerHTML = experiences[lang][key].date;
   });
 }
 
@@ -24,10 +88,9 @@ const btns = header.getElementsByClassName("company");
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
     const current = document.getElementsByClassName("activeExperience");
-    current[0].className = current[0].className.replace(
-      " activeExperience",
-      ""
-    );
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" activeExperience", "");
+    }
     this.className += " activeExperience";
   });
 }
